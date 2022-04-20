f5b3a08453e009223e12b2605392f2f4e7e268df
roseaugusto <rose.augusto@sun-asterisk.com>
2022-04-20 22:39:45 +0800

    Project Setup

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      {' '}
+      Welcome!
+      {/* Divide here per section */}
+    </div>
+  );
+};
+
+export default Home;
