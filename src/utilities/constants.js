629587a2b6949ae3f95d78daedfabf45ee02cb1b
roseaugusto <rose.augusto@sun-asterisk.com>
2022-04-27 16:22:45 +0800

    Refactor 1

---
+++ src/utilities/constants.js
@@ -0,0 +1,34 @@
+export const features = [
+  {
+    name: 'profile',
+    title: 'Let them know you',
+    isReverse: false,
+    src: '/images/FeatureProfile.png',
+    description:
+      'Flex your best qualities on your profile. Show your interests. Let it scream, “I’m the best fur parent in the whole world!”.',
+  },
+  {
+    name: 'match',
+    title: 'Match with your perfect buddy',
+    isReverse: true,
+    src: '/images/FeatureMatch.png',
+    description:
+      'Swipe to the right for the pet you like until you get your match.',
+  },
+  {
+    name: 'chat',
+    title: ' Get to know them',
+    isReverse: false,
+    src: '/images/FeatureChat.png',
+    description:
+      'Chat with the keeper of your match pet, and learn more about them.',
+  },
+  {
+    name: 'adopt',
+    title: 'Adopt',
+    isReverse: true,
+    src: '/images/FeatureAdopt.png',
+    description:
+      'Adopt, don’t shop. Make a deal with the pet keeper, and become a certified fur parent.',
+  },
+];
