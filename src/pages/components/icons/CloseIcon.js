fb3de036158ee4b1e9efc1da1978c4c8f2390146
Mary Therese S. Alegre <marytherese.alegre@sun-asterisk.com>
2022-04-27 14:59:58 +0800

    Added a background color on hover for the close icon

--- src/pages/components/icons/CloseIcon.js
+++ src/pages/components/icons/CloseIcon.js
@@ -5,7 +5,7 @@ const CloseIcon = ({ closeChatBox }) => {
     <svg
       onClick={closeChatBox}
       xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6 text-white cursor-pointer"
+      className="h-8 w-8 text-white cursor-pointer p-1 rounded-lg hover:bg-stone-300"
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
