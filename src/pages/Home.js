9818bdddd240f3a0f9d50ea5e7c525dbf7435393
roseaugusto <rose.augusto@sun-asterisk.com>
2022-04-28 15:46:49 +0800

    [Task] Refactor file structure

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import MainHeadline from './components/MainHeadline';
 import ContactUs from './components/ContactUs';
-import FeatureSection from './components/feature/FeatureSection';
-import Testimonial from './components/testimonial/Testimonial';
+import FeatureSection from './components/feature';
+import Testimonial from './components/testimonial';
 
 const Home = () => {
   return (
