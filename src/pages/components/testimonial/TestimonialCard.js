081c06a7f3f83fef1dd37fb9c042254e9b05d3e8
Joash Cañete <joash.canete@sun-asterisk.com>
2022-05-02 18:36:19 +0800

    [improvements]-testionials_cards-fixCard

--- src/pages/components/testimonial/TestimonialCard.js
+++ src/pages/components/testimonial/TestimonialCard.js
@@ -10,7 +10,7 @@ const TestimonialCard = ({ item }) => {
         alt={item.name}
       />
       {/* Content */}
-      <figcaption className='absolute left-0 right-0 bottom-5 mx-5 pb-5 bg-gradient-to-t from-black rounded-lg text-white'>
+      <figcaption className='absolute left-0 right-0 bottom-5 mr-5 pb-5 bg-gradient-to-t from-black rounded-lg text-white'>
         <p className='mb-3 mx-5 font-semibold'>{item.text}</p>
         <p className='mx-5 font-allison text-[32px]'>{item.author}</p>
       </figcaption>
