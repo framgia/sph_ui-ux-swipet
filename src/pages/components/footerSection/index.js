bb2355ab748f09f420c655abd0bb9fa0f9e5bc9b
roseaugusto <rose.augusto@sun-asterisk.com>
2022-05-05 17:41:14 +0800

    [Bugfix] Fix side margins on mobile

--- src/pages/components/footerSection/index.js
+++ src/pages/components/footerSection/index.js
@@ -5,15 +5,13 @@ import FooterContent from './FooterContent';
 const FooterSection = () => {
   return (
     <div>
-      <div className='bg-gradient-to-t from-orange-900 via-orange-600 to-orange-500 h-[420px] lg:h-[420px]'>
-        <div className="bg-[url('/public/images/BottomBanner.svg')] w-full h-full flex justify-center items-center flex-col gap-20">
-          <div>
-            <p className='leading-[65px] text-center text-5xl text-white font-bold'>
-              Your <span className='text-yellow-900'>bestfriend</span> is <br />
-              waiting for <span className='text-yellow-900'>you</span>
-            </p>
-          </div>
-          <div className='flex justify-center space-x-3 lg:space-x-4'>
+      <div className='bg-gradient-to-t from-orange-900 via-orange-600 to-orange-500 h-[420px] lg:h-[420px] mx-2'>
+        <div className="bg-[url('/public/images/BottomBanner.svg')] w-full h-full flex justify-center items-center flex-col gap-10 sm:gap-20">
+          <p className='leading-[50px] sm:leading-[65px] text-center text-4xl sm:text-5xl text-white font-bold '>
+            Your <span className='text-yellow-900'>bestfriend</span> is <br />
+            waiting for <span className='text-yellow-900'>you</span>
+          </p>
+          <div className='flex flex-col sm:flex-row justify-center space-x-0 space-y-3 sm:space-y-0 sm:space-x-3 lg:space-x-4'>
             <DownloadButton
               src='./images/GooglePlay.svg'
               alt='google play store'
