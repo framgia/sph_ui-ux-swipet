a0aeba6155f08c6fb86938ef886a0c2afdf9eee5
ErickDoroy <89514595+ErickDoroy@users.noreply.github.com>
2022-05-11 13:46:54 +0800

    [Bugfix] Header Picture is not tilted (#25)

--- src/pages/components/ScrollToTop.js
+++ src/pages/components/ScrollToTop.js
@@ -1,4 +1,4 @@
-import { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { useLocation } from 'react-router';
 
 const ScrollToTop = ({ children }) => {
