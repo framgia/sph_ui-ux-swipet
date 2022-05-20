c6d5b7a3ef828aba29a5868798a0bac9375c1782
ErickDoroy <89514595+ErickDoroy@users.noreply.github.com>
2022-05-20 13:38:25 +0800

    rebase

--- src/pages/components/externalPages/TestimonialExternalPages/TestimonialContentTemplate.js
+++ src/pages/components/externalPages/TestimonialExternalPages/TestimonialContentTemplate.js
@@ -9,20 +9,21 @@ const TestimonialContentTemplate = ({ mode, description }) => (
         mode === 'light' ? '' : 'bg-dark-ash-800'
       } flex px-[20px] pt-[64px] sm:px-[55px] sm:pt-[38px] lg:px-[100px] lg:pt-[51px] xl:px-[200px] 2xl:px-[339px]`}
     >
-      <LazyLoadImage src='/images/Avatar01.svg' alt='profile' className='h-[71px]' />
+      <LazyLoadImage
+        src={`${mode === 'light' ? '/images/Avatar01.svg' : '/images/Avatar01dark.svg'}`}
+        alt='profile'
+        className='h-[71px]'
+      />
       <div className='ml-7'>
-        <h1 className='text-[32px] text-navy-900'>{description.author}</h1>
+        <h1 className='text-[32px] text-navy-900 dark:text-white '>{description.author}</h1>
         <p>{description.posted}</p>
       </div>
     </header>
     <main
       className={`${mode === 'light' ? '' : 'bg-dark-ash-800'} border-brown-90 border-b  px-[20px]
-          text-lg text-brown-900 sm:px-[55px] sm:pt-[38px] md:px-[50px] lg:px-[100px] lg:pt-[51px] xl:px-[200px] 2xl:px-[339px]`}
+          text-lg text-brown-900 dark:text-white sm:px-[55px] sm:pt-[38px] md:px-[50px] lg:px-[100px] lg:pt-[51px] xl:px-[200px] 2xl:px-[339px]`}
     >
-      <TestimonialMainContent
-        description={description.description}
-        altImage={description.author}
-      />
+      <TestimonialMainContent description={description.description} altImage={description.author} />
     </main>
   </Fragment>
 );
--- src/pages/components/externalPages/TestimonialExternalPages/TestimonialHeaderContent.js
+++ src/pages/components/externalPages/TestimonialExternalPages/TestimonialHeaderContent.js
@@ -17,46 +17,53 @@ const TestimonialHeaderContent = ({ mode, item }) => (
       className='absolute left-0 right-0 bottom-5 h-full bg-gradient-to-r from-orange-900 dark:from-dark-ash-900
       '
     >
-      <div className='px-[20px] pt-[64px] sm:px-[55px] sm:pt-[38px] lg:h-[572px] lg:px-[100px] lg:pt-[51px] xl:px-[200px] 2xl:px-[339px]'>
-        <Link to='/'>
-          <div className='h-[39px] w-[165px] hover:cursor-pointer sm:m-auto lg:h-[48px] lg:w-[192px]'>
-            <LazyLoadImage
-              src={`${mode === 'light' ? '/images/LogoMix.svg' : '/images/LogoDarkTheme.svg'}`}
-              alt='swipet logo'
-            />
-          </div>
-        </Link>
-        <div className='relative flex flex-col items-start'>
-          <h1
-            className='mt-[30px] flex flex-col self-start pb-5 text-[40px] font-bold leading-[40px] text-white
-                      dark:text-gray-100 sm:flex-row sm:self-auto lg:mt-[77px] lg:flex-col lg:pb-4 lg:text-5xl 2xl:text-[54px]'
-          >
-            {item.text}
-          </h1>
-          <h3
-            className='py-11 font-allison text-[64px] leading-7 text-yellow-900 dark:text-gray-100
+      <LazyLoadImage
+        src={item.src}
+        className='h-full w-full object-cover md:object-top lg:object-[center_-400px]'
+        alt={item.name}
+      />
+      <figcaption className='absolute left-0 right-0 bottom-5 h-full bg-gradient-to-r from-orange-900 dark:from-dark-ash-900'>
+        <div className='px-[20px] pt-[64px] sm:px-[55px] sm:pt-[38px] lg:h-[572px] lg:px-[100px] lg:pt-[51px] xl:px-[200px] 2xl:px-[339px]'>
+          <Link to='/'>
+            <div className='h-[39px] w-[165px] hover:cursor-pointer sm:m-auto lg:h-[48px] lg:w-[192px]'>
+              <LazyLoadImage
+                src={`${mode === 'light' ? '/images/LogoMix.svg' : '/images/LogoDarkTheme.svg'}`}
+                alt='swipet logo'
+              />
+            </div>
+          </Link>
+          <div className='relative flex flex-col items-start'>
+            <h1
+              className='mt-[30px] flex flex-col self-start pb-5 text-[40px] font-bold leading-[40px] text-white
+                       sm:flex-row sm:self-auto lg:mt-[77px] lg:flex-col lg:pb-4 lg:text-5xl 2xl:text-[54px]'
+            >
+              {item.text}
+            </h1>
+            <h3
+              className='py-11 font-allison text-[64px] leading-7 text-yellow-900 dark:text-white
                       sm:py-[35px] sm:text-center lg:w-[400px] lg:pb-12 lg:text-left xl:w-[545px] xl:text-[64px] 2xl:w-[580px]'
-          >
-            {item.author}
-          </h3>
-          <div className='flex justify-center space-x-3 pb-11 lg:space-x-4'>
-            <DownloadButton
-              src='../images/GooglePlay.svg'
-              alt='google play store'
-              upperText='ANDROID APP ON'
-              lowerText='Google Play'
-              link='https://play.google.com/store'
-            />
-            <DownloadButton
-              src='../images/AppStore.svg'
-              alt='apple app store'
-              upperText='DOWNLOAD ON THE'
-              lowerText='App Store'
-              link='https://www.apple.com/ph/app-store/'
-            />
+            >
+              {item.author}
+            </h3>
+            <div className='flex justify-center space-x-3 pb-11 lg:space-x-4'>
+              <DownloadButton
+                src='../images/GooglePlay.svg'
+                alt='google play store'
+                upperText='ANDROID APP ON'
+                lowerText='Google Play'
+                link='https://play.google.com/store'
+              />
+              <DownloadButton
+                src='../images/AppStore.svg'
+                alt='apple app store'
+                upperText='DOWNLOAD ON THE'
+                lowerText='App Store'
+                link='https://www.apple.com/ph/app-store/'
+              />
+            </div>
           </div>
         </div>
-      </div>
+      </figcaption>
     </figcaption>
   </figure>
 );
