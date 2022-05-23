9d88ad6af51414479cb6fd4a3576c2eb42b8eaea
Joash Cañete <89382909+CaneteJoash@users.noreply.github.com>
2022-05-23 16:13:27 +0800

    [bugTask]-404_page_in_testimonial_external_pages (#41)
    * [bugTask]-404_page_in_testimonial_external_pages
    
    * [bugTask]-404_page_in_testimonial_external_page
    
    * [bugTask]-404_page_in_testimonial_external_page-rebase

--- src/pages/components/externalPages/TestimonialExternalPages/TestimonialContent.js
+++ src/pages/components/externalPages/TestimonialExternalPages/TestimonialContent.js
@@ -8,6 +8,7 @@ import FooterContent from '../../footerSection/FooterContent';
 import CookiePolicy from '../../CookiePolicy';
 import ContactUs from '../../chatbot';
 import useDarkMode from '../../../../utilities/hooks/useDarkMode';
+import NotFound from '../NotFound';
 
 const TestimonialContent = () => {
   const { name } = useParams('name');
@@ -16,12 +17,18 @@ const TestimonialContent = () => {
 
   return (
     <div className='dark:bg-dark-ash-800'>
-      <ContactUs />
-      <TestimonialHeaderContent item={authorData} mode={mode} />
-      <TestimonialContentTemplate description={authorData} mode={mode} />
-      <Testimonial testimonialCaption='You may also like' />
-      <FooterContent mode={mode} />
-      <CookiePolicy />
+      {authorData === undefined ? (
+        <NotFound />
+      ) : (
+        <>
+          <ContactUs />
+          <TestimonialHeaderContent item={authorData} mode={mode} />
+          <TestimonialContentTemplate description={authorData} mode={mode} />
+          <Testimonial testimonialCaption='You may also like' />
+          <FooterContent mode={mode} />
+          <CookiePolicy />
+        </>
+      )}
     </div>
   );
 };
