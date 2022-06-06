89c8aaee3cb7ca018b69d78f6bcee82e5c36de74
Egie Garciano <egie.garciano@sun-asterisk.com>
2022-06-06 16:26:40 +0800

    [Task] Add Counter Up in Feature Details Page

--- src/pages/components/summarySection/SummaryCard.js
+++ src/pages/components/summarySection/SummaryCard.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useSpring, animated } from '@react-spring/web';
+import { useInView } from 'react-intersection-observer';
 import DogIcon from '../icons/DogIcon';
 import CatIcon from '../icons/CatIcon';
 import PetParentIcon from '../icons/PetParentIcon';
 import PetMatchIcon from '../icons/PetMatchIcon';
 
 const SummaryCard = ({ summary }) => {
+  const { ref, inView } = useInView();
+
+  const props = useSpring({ count: inView ? summary.count : 0, config: { duration: 2000 } });
+
   const iconComponent = () => {
     switch (summary.name) {
       case 'dog':
@@ -23,7 +29,9 @@ const SummaryCard = ({ summary }) => {
   return (
     <div className='flex h-[200px] w-[227px] flex-col items-center rounded-lg bg-sky-300 py-4 px-2 text-center sm:w-full'>
       <div className='mb-6'>{iconComponent()}</div>
-      <p className='mb-[26px] text-2xl font-semibold text-navy-900'>{summary.count}</p>
+      <animated.p ref={ref} className='mb-[26px] text-2xl font-semibold text-navy-900'>
+        {props.count.to((x) => new Intl.NumberFormat().format(Math.floor(x)))}
+      </animated.p>
       <p className='font-medium text-brown-900'>{summary.title}</p>
     </div>
   );
