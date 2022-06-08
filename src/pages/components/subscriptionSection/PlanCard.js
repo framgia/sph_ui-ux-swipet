5d5a8aa21aa18081bc63af254bb44ffb28dd4d14
Mary Therese S. Alegre <marytherese.alegre@sun-asterisk.com>
2022-06-08 09:53:38 +0800

    Removed the dot and self closed the div tag

--- src/pages/components/subscriptionSection/PlanCard.js
+++ src/pages/components/subscriptionSection/PlanCard.js
@@ -11,7 +11,7 @@ const PlanCard = ({ subscription }) => (
       <ul className='list-none divide-y divide-orange-900 px-9 py-2 dark:divide-gray-500'>
         {subscription.content.map((feature, index) => (
           <li className='flex py-4' key={index}>
-            <div className='relative left-5 top-1 h-4 w-4 rounded-full bg-white'>.</div>
+            <d className='relative left-5 top-1 h-4 w-4 rounded-full bg-white' />
             {feature.included ? <CheckCirledIcon /> : <CloseCirledIcon />}
             <span className='ml-2 dark:text-white'>{feature.description}</span>
           </li>
--- src/pages/components/tipSection/Tips.js
+++ src/pages/components/tipSection/Tips.js
@@ -7,7 +7,7 @@ const Tips = () => (
     <ul className='mb-7 list-none px-9 py-2'>
       {tips.map((tip, index) => (
         <li className='flex flex-row py-4' key={index}>
-          <div className='relative left-5 top-1 h-4 w-4 rounded-full bg-white'>.</div>
+          <div className='relative left-5 top-1 h-4 w-4 rounded-full bg-white' />
           <CheckCirledIcon />
           <span className='text ml-2 flex-1 text-2xl text-brown-900 dark:text-white'>{tip}</span>
         </li>
