815a18add7dcd351739901bb8f9189fc7d4df0af
Egie Garciano <egie.garciano@sun-asterisk.com>
2022-06-08 18:11:02 +0800

    [Bugfix] Duplicate text is displayed

--- src/pages/components/externalPages/TermsAndConditionsPage.js
+++ src/pages/components/externalPages/TermsAndConditionsPage.js
@@ -7,25 +7,43 @@ import { termServiceMetaData } from '../../../utilities/constants';
 const TermsAndConditionsPage = () => {
   const { t } = useTranslation();
   const termsAndConditions = t('termsAndConditions', { ns: 'informations' });
-
   const allUlTags = termsAndConditions.filter((list) => list.isUlTag === true);
-
   const allListText = allUlTags.map((list) => list.text);
 
-  let listItem;
+  const listItems = [];
   for (let i = 0; i < allListText.length; i++) {
-    listItem = allListText[i].map((list, index) => <li key={index}>{list}</li>);
+    const listItem = allListText[i].map((list, index) => <li key={index}>{list}</li>);
+    listItems.push(listItem);
   }
+
+  const checkIndex = (i) => {
+    switch (i) {
+      case 10:
+        return 0;
+      case 15:
+        return 1;
+      case 19:
+        return 2;
+      case 22:
+        return 3;
+      case 27:
+        return 4;
+      case 42:
+        return 5;
+      default:
+        return null;
+    }
+  };
 
   return (
     <ExternalPageTemplate title={t('terms&Conditions')}>
       <MetaDecorator title={termServiceMetaData.title} content={termServiceMetaData.content} />
 
-      {termsAndConditions.map((item, index) => (
-        <div key={index}>
+      {termsAndConditions.map((item, i) => (
+        <div key={i}>
           {item.isPTag && <p className='pb-4 text-lg'>{item.text}</p>}
           {item.isH3Tag && <h3 className='pb-4 text-2xl font-medium'>{item.text}</h3>}
-          {item.isUlTag && <ul className='list-disc pl-8 pb-4'>{listItem}</ul>}
+          {item.isUlTag && <ul className='list-disc pl-8 pb-4'>{listItems[checkIndex(i)]}</ul>}
         </div>
       ))}
     </ExternalPageTemplate>
--- src/pages/components/externalPages/TestimonialExternalPages/TestimonialMainContent.js
+++ src/pages/components/externalPages/TestimonialExternalPages/TestimonialMainContent.js
@@ -3,21 +3,38 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const TestimonialMainContent = ({ description, altImage }) => {
   const allUlTags = description.filter((list) => list.isUlTag === true);
-
   const allListText = allUlTags.map((list) => list.text);
-  let listItem;
+
+  const listItems = [];
   for (let i = 0; i < allListText.length; i++) {
-    listItem = allListText[i].map((list, index) => <li key={index}>{list}</li>);
+    const listItem = allListText[i].map((list, index) => <li key={index}>{list}</li>);
+    listItems.push(listItem);
   }
+
+  const checkIndex = (i) => {
+    switch (i) {
+      case 4:
+        return 0;
+      case 10:
+        return 0;
+      case 12:
+        return 1;
+      case 13:
+        return 1;
+      default:
+        return null;
+    }
+  };
+
   return (
     <main className='pb-[51px] text-brown-900 dark:text-white'>
-      {description.map((item, index) => (
-        <div key={index}>
+      {description.map((item, i) => (
+        <div key={i}>
           {item.isH1Tag && <h1 className='py-4 text-[40px] font-bold leading-none'>{item.text}</h1>}
           {item.isH4Tag && <h4 className='pt-4 text-xl font-semibold'>{item.text}</h4>}
           {item.isH4PTag && <p className='pb-4 text-lg'>{item.text}</p>}
           {item.isPTag && <p className='py-4 text-lg'>{item.text}</p>}
-          {item.isUlTag && <ul className='list-disc pl-8 pb-4'>{listItem}</ul>}
+          {item.isUlTag && <ul className='list-disc pl-8 pb-4'>{listItems[checkIndex(i)]}</ul>}
           {item.isImgTag && (
             <LazyLoadImage
               src={item.src}
