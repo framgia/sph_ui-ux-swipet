9ef8b8d332fdecf555695d62949af8c47da7981f
Joash Cañete <89382909+CaneteJoash@users.noreply.github.com>
2022-06-10 13:21:51 +0800

    [task]-Feature_detail_demo_section (#54)
    * [task]-Feature_detail_demo_section
    
    * Clean up

--- src/pages/components/tipSection/index.js
+++ src/pages/components/tipSection/index.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Tips from './Tips';
 import IdeaIcon from '../icons/IdeaIcon';
+import Demo from './Demo';
 
 const TipSection = () => {
   const { t } = useTranslation('features');
+
+  const [isDemo, setIsDemo] = useState(false);
+  const [isActive, setIsActive] = useState(true);
+  const [isNotActive, setIsNotActive] = useState(true);
+
+  useEffect(() => {
+    if (!isDemo) {
+      setIsActive(
+        'h-[38px] w-20 rounded-l-lg bg-navy-600 px-2 py-1 text-lg font-semibold text-white hover:underline focus:bg-navy-600 focus:text-white',
+      );
+    } else {
+      setIsActive(
+        'h-[38px] w-20 rounded-l-lg bg-navy-100 px-2 py-1 text-lg font-semibold text-dark-ash-900 hover:underline focus:bg-navy-600 focus:text-white',
+      );
+    }
+  }, [isDemo]);
+
+  useEffect(() => {
+    if (isDemo) {
+      setIsNotActive(
+        'h-[38px] w-20 rounded-r-lg bg-navy-600 px-2 py-1 text-lg font-semibold text-white hover:underline focus:bg-navy-600 focus:text-white',
+      );
+    } else {
+      setIsNotActive(
+        'h-[38px] w-20 rounded-r-lg bg-navy-100 px-2 py-1 text-lg font-semibold text-dark-ash-900 hover:underline focus:bg-navy-600 focus:text-white',
+      );
+    }
+  }, [isDemo]);
 
   return (
     <section className=' bg-white py-28 px-5 dark:bg-dark-ash-700 xl:px-[240px]'>
@@ -31,18 +60,16 @@ const TipSection = () => {
         </div>
 
         {/* Button */}
-        <div className='flex flex-1 flex-col justify-center'>
-          <div className='flex justify-center pb-[30px] sm:pb-[55px]'>
-            <button className='h-[38px] w-20 rounded-l-lg bg-navy-600 px-2 py-1 text-lg font-semibold text-white hover:underline dark:bg-orange-900'>
+        <div className='justify-top flex flex-1 flex-col '>
+          <div className='flex justify-center pb-[30px] pt-4 sm:pb-[35px]'>
+            <button className={`${isActive}`} onClick={() => setIsDemo(false)}>
               {t('tipsBtnOne')}
             </button>
-            <button className='h-[38px] w-20 rounded-r-lg bg-navy-100 px-2 py-1 text-lg font-semibold text-dark-ash-900 hover:underline dark:bg-orange-200'>
+            <button className={`${isNotActive}`} onClick={() => setIsDemo(true)}>
               {t('tipsBtnTwo')}
             </button>
           </div>
-          <div className='flex flex-col items-center md:px-14'>
-            <Tips />
-          </div>
+          <div className='flex flex-col items-center md:px-14'>{isDemo ? <Demo /> : <Tips />}</div>
         </div>
       </div>
     </section>
