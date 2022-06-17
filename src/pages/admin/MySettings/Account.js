7f5d4fac08de1ddfd4eb67afd94e104519ff21e9
Joash Cañete <89382909+CaneteJoash@users.noreply.github.com>
2022-06-17 15:26:19 +0800

    Task/Admin  My settings account tab (#68)
    * Task/Admin  My settings account tab
    
    * clean up
    
    * remove placeholder

--- src/pages/admin/MySettings/Account.js
+++ src/pages/admin/MySettings/Account.js
@@ -1,5 +1,47 @@
 import React from 'react';
+import Button from '../../components/shared/Button';
+import InputField from '../../components/shared/InputField';
 
-const AdminAccountTab = () => <div>Account</div>;
+const AdminAccountTab = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault(e.target.value);
+    window.location = '/admin/login';
+  };
+
+  const handleOnChange = () => {};
+
+  return (
+    <div className='relative text-brown-900'>
+      <form onSubmit={handleSubmit}>
+        <div className='text-xl font-bold'>CHANGE PASSWORD</div>
+        <div className='w-[455px] px-6'>
+          <InputField
+            name='Current Password'
+            type='password'
+            value='currentPassword'
+            onChange={handleOnChange}
+          />
+          <InputField
+            name='New Password'
+            type='password'
+            value='newPassword'
+            onChange={handleOnChange}
+          />
+          <InputField
+            name='Confirm New Password'
+            type='password'
+            value='confirmNewPassword'
+            onChange={handleOnChange}
+          />
+        </div>
+        <Button
+          type='submit'
+          buttonLabel='Save Changes'
+          className='absolute -bottom-44 -left-10 w-[176px]'
+        />
+      </form>
+    </div>
+  );
+};
 
 export default AdminAccountTab;
