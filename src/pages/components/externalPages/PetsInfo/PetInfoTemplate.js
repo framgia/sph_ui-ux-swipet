e42896686321ab453f7d476a495b48cd5104ad7f
Joash Cañete <joash.canete@sun-asterisk.com>
2022-06-28 11:47:26 +0800

    task/Implement dark theme in pets info page

--- src/pages/components/externalPages/PetsInfo/PetInfoTemplate.js
+++ src/pages/components/externalPages/PetsInfo/PetInfoTemplate.js
@@ -3,18 +3,12 @@ import PetsDetails from './PetsDetails';
 import PetsMyInfo from './PetsMyInfo';
 
 const PetInfoTemplate = ({ data }) => (
-  <div>
-    <section
-      className='px-5 pt-14 dark:bg-dark-ash-900
-    dark:bg-none sm:px-8 md:px-14 xl:px-[200px] 2xl:px-[240px] 3xl:px-[320px]'
-    >
+  <div className='dark:bg-dark-ash-800'>
+    <section className='px-5 pt-14 sm:px-8 md:px-14 xl:px-[200px] 2xl:px-[240px] 3xl:px-[320px]'>
       <PetsDetails getData={data} />
     </section>
-    <hr className='border border-orange-900' />
-    <section
-      className='px-5 pt-14 dark:bg-dark-ash-900
-    dark:bg-none sm:px-8 md:px-14 xl:px-[200px] 2xl:px-[240px] 3xl:px-[320px]'
-    >
+    <hr className='border border-orange-900 dark:border-white' />
+    <section className='px-5 pt-14 sm:px-8 md:px-14 xl:px-[200px] 2xl:px-[240px] 3xl:px-[320px]'>
       <PetsMyInfo getData={data.myInfo} />
     </section>
   </div>
--- src/pages/components/externalPages/PetsInfo/PetsMyInfo.js
+++ src/pages/components/externalPages/PetsInfo/PetsMyInfo.js
@@ -3,10 +3,12 @@ import CheckCircledIcon from '../../icons/CheckCircledIcon';
 
 const PetsMyInfo = ({ getData }) => (
   <div>
-    <h1 className='mb-[70px] text-[48px] font-semibold text-brown-900'>My info</h1>
+    <h1 className='mb-[70px] text-[48px] font-semibold text-brown-900 dark:text-orange-900'>
+      My info
+    </h1>
     <div className='grid grid-flow-col grid-rows-3 gap-4 pl-[64px] pb-[123px] text-[16px] font-bold'>
       {getData.map((item, idx) => (
-        <div className='flex gap-[9px]' key={idx}>
+        <div className='flex gap-[9px] dark:text-white' key={idx}>
           <CheckCircledIcon /> <span>{item}</span>
         </div>
       ))}
